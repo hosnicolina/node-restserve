d381c87883b14976adb11d7d3c1d399ab1198d7c
hosni Colina <corobonhosni@gmail.com>
2018-08-08 05:44:39 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
  if (process.env.NODE_ENV === 'dev') {
    urlDB = 'mongodb://localhost:27017/cafe'
  }else{
-   urlDB = 'mongodb://cafe-user:hosni.1991@ds115442.mlab.com:15442/cafe'
+   urlDB = process.env.MONGO_URi
  }
 
  process.env.URLDB = urlDB
